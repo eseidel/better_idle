97772ca4e6651cec2a21a1d385913757f1fe20b6
Eric Seidel <github@eseidel.com>
2025-12-21 12:43:43 -0800

    chore: remove globals

--- logic/lib/src/data/registries.dart
+++ logic/lib/src/data/registries.dart
@@ -11,30 +11,18 @@ class Registries {
   final DropsRegistry drops;
 }
 
-Registries? _registries;
-
 /// Ensures the registries are initialized.
 ///
 /// This should be called during app startup or in setUpAll() for tests.
 /// It's safe to call multiple times; subsequent calls are no-ops.
 Future<Registries> loadRegistries({Directory? cacheDir}) async {
-  if (_registries != null) return _registries!;
   final melvorData = await MelvorData.load(cacheDir: cacheDir);
-  _registries = Registries(
+  return Registries(
     initializeItems(melvorData),
     ActionRegistry(loadActions(melvorData)),
     DropsRegistry(skillDrops, globalDrops),
   );
-  return _registries!;
 }
-
-// /// Returns the loaded registries. Throws if not yet loaded.
-// Registries get registries {
-//   if (_registries == null) {
-//     throw StateError('Registries not loaded. Call loadRegistries() first.');
-//   }
-//   return _registries!;
-// }
 
 ItemRegistry initializeItems(MelvorData data) {
   final items = <Item>[];
